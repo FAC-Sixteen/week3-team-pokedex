b8e0e3cd39eb213aedbae6c24c7633e33539b1e3
banna <bannatekle@gmail.com>
2019-03-21 22:40:29 +0000

    audio cntrols finalised

--- public/audio_controls.js
+++ public/audio_controls.js
@@ -1,6 +1,12 @@
 let controls = document.getElementById('player')
+let symbol = document.querySelector('.controlsSymbol')
 
 function togglePlay(){
-    
-    return controls.paused ? controls.play() : controls.pause();
+    if(controls.paused){
+        symbol.src = 'iconfinder_icon-ios7-pause_211791.png'
+        return controls.play();
+    } else{
+        symbol.src = 'iconfinder_icon-play_211876.png'
+        return controls.pause();
+    }
 }
