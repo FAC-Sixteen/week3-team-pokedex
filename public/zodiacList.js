7fdc45fe2c8d797e2cc813fd5d78001e896fb28e
Rohan Shah Summers <rohan_shah_summers@yahoo.co.uk>
2019-03-22 12:10:46 +0000

    added cors for http/https hack

--- public/zodiacList.js
+++ public/zodiacList.js
@@ -1,23 +1,25 @@
 // fetch from API on load event, creates sign object.
 
-fetch('http://sandipbgt.com/theastrologer/api/sunsigns/', function(response){
-      response.map((element, i) => {
-        signs[i] = element;
+fetch(
+  "https://cors-anywhere.herokuapp.com/http://sandipbgt.com/theastrologer/api/sunsigns/",
+  function(response) {
+    response.map((element, i) => {
+      signs[i] = element;
     });
 
-// populate the html with signs object + aria labels for respective option
+    // populate the html with signs object + aria labels for respective option
     const form = document.querySelector("select");
 
-    for (let i = 0; i < Object.keys(signs).length; i++){
-        let listItem = document.createElement('option');
-        listItem.textContent = signs[i];
-        listItem.setAttribute("aria-label","list option: "+listItem.textContent);
-        form.appendChild(listItem);
+    for (let i = 0; i < Object.keys(signs).length; i++) {
+      let listItem = document.createElement("option");
+      listItem.textContent = signs[i];
+      listItem.setAttribute(
+        "aria-label",
+        "list option: " + listItem.textContent
+      );
+      form.appendChild(listItem);
     }
-});
+  }
+);
 
 const signs = {};
-
-
-
-
